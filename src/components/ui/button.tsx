db45b4f9749c749ba78ef0811d7d901462642f98
manuelluvuvamo <manuelluvuvamo337@gmail.com>
2023-10-25 17:40:32 +0100

    main 25.10.23 17:40

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+/* import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
@@ -54,3 +54,4 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 Button.displayName = "Button"
 
 export { Button, buttonVariants }
+ */
