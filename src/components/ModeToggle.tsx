30c4ce4720e7553c051d478d4a10f48301cc5d19
Manuel Luvuvamo <manuel.luvuvamo@kinsari.com>
2024-03-11 09:59:57 +0100

    Novos CVs e automatização do ano no footer

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -25,13 +25,13 @@ export function ModeToggle() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="bg-white dark:bg-zinc-800">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
+        <DropdownMenuItem onClick={() => setTheme("light")} className="cursor-pointer">
           Light
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
+        <DropdownMenuItem onClick={() => setTheme("dark")} className="cursor-pointer">
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
+        <DropdownMenuItem onClick={() => setTheme("system")} className="cursor-pointer">
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
